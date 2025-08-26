2c3900e36441c757b5786458019e916769ea8086
Reinaldoe <Reinaldoescobedo43@gmail.com>
2025-08-26 12:24:40 -0400

    Tareaterminada

--- task4.js
+++ task4.js
@@ -1,7 +1,13 @@
 export class Player {
-  constructor() {
-   
+  constructor(name, level) {
+    this.name = name;  // Almacena el nombre del jugador en la propiedad 'name'
+    this.level = level; // Almacena el nivel del jugador en la propiedad 'level'
   }
-  
-  
+  info() {
+    console.log(`${this.name} has reached Level ${this.level}!`); // Imprime el mensaje en la consola
+  }
+  levelUp () {
+    this.level += 1 ; //sube de nivel
+    console.log(`${this.name} leveled up! Now at Level ${this.level}.`); // Mensaje al subir de nivel
+  }
 }
